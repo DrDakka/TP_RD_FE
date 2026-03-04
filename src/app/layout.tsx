7339efd91ba3cb95697a0a6ad3323b8eb1fb565b
Bespalov Tymofii <drdakkadakka@gmail.com>
2026-03-04 15:43:36 +0100

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from 'next';
+import { IBM_Plex_Mono, Outfit } from 'next/font/google';
 import './globals.scss';
 import Header from '@widgets/header/Header';
+
+const outfit = Outfit({
+  subsets: ['latin'],
+  variable: '--font-outfit',
+});
+
+const ibmPlexMono = IBM_Plex_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-mono',
+});
 
 export const metadata: Metadata = {
   title: 'TP RD',
@@ -14,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <body>
+      <body className={`${outfit.variable} ${ibmPlexMono.variable}`}>
         <Header />
         {children}
         <footer></footer>

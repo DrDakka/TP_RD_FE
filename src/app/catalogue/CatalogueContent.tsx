a772cd67e76c957399bacb600b205614ec4f8e69
Bespalov Tymofii <drdakkadakka@gmail.com>
2026-03-23 21:23:20 +0100

    add suspence to urlReducer

---
+++ src/app/catalogue/CatalogueContent.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useUrlReducer } from './useUrlReducer';
+import { Tags, PropTags } from '@/shared/api/types/product/enums';
+
+export const CatalogueContent = () => {
+  const [state, dispatch] = useUrlReducer();
+
+  return (
+    <div>
+      <p>search: {state.search}</p>
+      <p>tag: {state.tag}</p>
+      <p>prop: {state.prop.join(', ')}</p>
+      <p>page: {state.page}</p>
+
+      <button onClick={() => dispatch({ type: 'SET_SEARCH', payload: 'test' })}>
+        Set search
+      </button>
+      <button onClick={() => dispatch({ type: 'SET_TAG', payload: Tags.BAL })}>
+        Set tag
+      </button>
+      <button
+        onClick={() => dispatch({ type: 'TOGGLE_PROP', payload: PropTags.HP })}
+      >
+        Toggle hi-prot
+      </button>
+      <button
+        onClick={() => dispatch({ type: 'SET_PAGE', payload: state.page + 1 })}
+      >
+        Next page
+      </button>
+      <button onClick={() => dispatch({ type: 'RESET_FILTERS' })}>Reset</button>
+    </div>
+  );
+};
--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -1,35 +1,10 @@
-'use client';
-
-import { useUrlReducer } from './useUrlReducer';
-import { Tags, PropTags } from '@/shared/api/types/product/enums';
+import { Suspense } from 'react';
+import { CatalogueContent } from './CatalogueContent';
 
 export default function CataloguePage() {
-  const [state, dispatch] = useUrlReducer();
-
   return (
-    <div>
-      <p>search: {state.search}</p>
-      <p>tag: {state.tag}</p>
-      <p>prop: {state.prop.join(', ')}</p>
-      <p>page: {state.page}</p>
-
-      <button onClick={() => dispatch({ type: 'SET_SEARCH', payload: 'test' })}>
-        Set search
-      </button>
-      <button onClick={() => dispatch({ type: 'SET_TAG', payload: Tags.BAL })}>
-        Set tag
-      </button>
-      <button
-        onClick={() => dispatch({ type: 'TOGGLE_PROP', payload: PropTags.HP })}
-      >
-        Toggle hi-prot
-      </button>
-      <button
-        onClick={() => dispatch({ type: 'SET_PAGE', payload: state.page + 1 })}
-      >
-        Next page
-      </button>
-      <button onClick={() => dispatch({ type: 'RESET_FILTERS' })}>Reset</button>
-    </div>
+    <Suspense>
+      <CatalogueContent />
+    </Suspense>
   );
 }

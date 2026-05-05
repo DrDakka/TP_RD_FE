f6e8b8fa0e8858004eab8129141b9e7f36fd1fc1
Bespalov Tymofii <drdakkadakka@gmail.com>
2026-05-05 16:10:49 +0200

    removed header 1.0

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -24,6 +24,7 @@ export const Header = () => {
     user,
   } = useHeader();
 
+  // eslint-disable-next-line no-console
   console.log(user);
 
   return (
--- src/widgets/header_old/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { NavBar } from './navBar/NavBar';
-export { BurgerButton } from './burgerButton/BurgerButton';
-export { SearchDropdown } from './searchDropdown/SearchDropdown';
-export { SearchBar } from './searchBar/SearchBar';
-export { ProfileMenu } from './profileMenu';

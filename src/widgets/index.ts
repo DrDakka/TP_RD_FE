a3a8a8f83b3cdbf2f9502182fd4d761e24d22250
Bespalov Tymofii <drdakkadakka@gmail.com>
2026-05-05 16:56:45 +0200

    rollback to stable v

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,4 +1,4 @@
-export { Header } from './header_old/Header';
+export { Header } from './header/Header';
 export { Footer } from './footer/Footer';
 export { TestWidget } from './test/Test';
 export { ProductCard } from './productCard/ProductCard';
